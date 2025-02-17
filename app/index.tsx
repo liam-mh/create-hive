b71ab38a18ce3dfab21bd46750a23b5f0c1013db
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-17 18:53:54 +0000

    feat/ index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import DisplayAllUsers from '@/components/DisplayAllUsers';
+import { StyleSheet, Text, View} from 'react-native';
+
 
 export default function Index() {
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world!</Text>
+      <DisplayAllUsers />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  }
+});
