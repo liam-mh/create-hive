efce085e073c62978d5ed3cc6cd54b1a4d9e1e05
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-18 00:47:32 +0000

    feat/ firebase auth

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,20 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 
 const firebaseConfig = {
-  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
-  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
-  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
-  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
-  measurementId: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID
+    apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+    authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+    projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+    storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+    appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
+    measurementId: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage) 
+});
+
 export const storage = getStorage(app);
