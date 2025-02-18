95ebd1eb30f62cf9c46b10559083c38a977dd848
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-18 21:13:06 +0000

    feat/ map with overlaying components

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,35 +1,17 @@
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Map from '@/components/Map';
-import DisplayAllUsers from '@/components/DisplayAllUsers';
 
 export default function Explore() {
-  const insets = useSafeAreaInsets();
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.mapContainer}> 
-                <Map /> 
-            </View>
-            <SafeAreaView style={styles.safeArea}> 
-                <DisplayAllUsers />
-            </SafeAreaView>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Map />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-  },
-  mapContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  safeArea: { 
-    flex: 1,
   },
 });
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,32 +1,46 @@
 import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TEXT, { SHADOWS, SIZES } from '@/styles';
 
 const Map = () => {
 
-    const INITIAL_REGION = {
-        latitude: 53.380871,
-        longitude: -1.4701,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05 
-    };
+  const INITIAL_REGION = {
+    latitude: 53.380871,
+    longitude: -1.4701,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05 
+  };
 
-    return (
-        <MapView 
-            style={styles.map} 
-            provider={PROVIDER_DEFAULT}
-            initialRegion={INITIAL_REGION}
-            showsUserLocation
-            showsMyLocationButton
-        />
-    );
+  return (
+    <MapView 
+      style={styles.map} 
+      provider={PROVIDER_DEFAULT}
+      initialRegion={INITIAL_REGION}
+      showsUserLocation
+      showsMyLocationButton
+    >
+      <SafeAreaView style={{ flex: 1 }} edges={['top']} pointerEvents="box-none">
+        <View style={styles.paddedContainer}>
+          <View style={[SHADOWS.containerShadow]}>
+            <Text style={TEXT.h1}>sheffield</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
-    map: {
-        flex: 1,
-        width: '100%', 
-        height: '100%',
-    }
+  map: {
+    flex: 1,
+    width: '100%', 
+    height: '100%',
+  },
+  paddedContainer: { 
+    paddingHorizontal: SIZES.m, 
+    paddingVertical: 0, 
+  },
 });
 
 export default Map;
