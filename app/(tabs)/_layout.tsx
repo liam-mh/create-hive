cefd5bed10676bf0ac339ad3a0c2de4f59c8dadb
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-19 18:21:44 +0000

    feat/ tabs + custom icon nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,16 @@
 import { Tabs } from "expo-router";
-import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { COLOURS, SIZES } from '@/styles';
+
+import IconHouse from '@/assets/icons/house-door.svg';
+import IconHouseFill from '@/assets/icons/house-door-fill.svg';
+import IconPin from '@/assets/icons/geo-alt.svg';
+import IconPinFill from '@/assets/icons/geo-alt-fill.svg';
+import IconSearch from '@/assets/icons/search.svg';
+import IconCreate from '@/assets/icons/plus-square.svg';
+import IconCreateFill from '@/assets/icons/plus-square-fill.svg';
+import IconPerson from '@/assets/icons/person.svg';
+import IconPersonFill from '@/assets/icons/person-fill.svg';
 
 export default function RootLayout() {
   return (
@@ -16,18 +26,62 @@ export default function RootLayout() {
         options={{ 
           title: 'Home', 
           tabBarIcon: ({ color, focused }) => (
-            <MaterialCommunityIcons name={focused ? 'home' : 'home-outline'} color={color} size={SIZES.l} />
+            focused ? 
+              <IconHouseFill width={SIZES.l} height={SIZES.l} fill={color} /> 
+              : 
+              <IconHouse width={SIZES.l} height={SIZES.l} fill={color} />
           ),
-        }}/>
+        }}
+      />
       <Tabs.Screen 
-        name="explore" 
+        name="search"
+        options={{ 
+          title: 'Search', 
+          tabBarIcon: ({ color, focused }) => (
+            focused ? 
+              <IconSearch width={SIZES.l} height={SIZES.l} fill={color} /> 
+              : 
+              <IconSearch width={SIZES.l} height={SIZES.l} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name="create"
+        options={{ 
+          title: 'Create', 
+          tabBarIcon: ({ color, focused }) => (
+            focused ? 
+              <IconCreateFill width={SIZES.l} height={SIZES.l} fill={color} /> 
+              : 
+              <IconCreate width={SIZES.l} height={SIZES.l} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name="explore"
         options={{ 
           title: 'Explore', 
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
-            <MaterialCommunityIcons name={focused ? 'map-marker' : 'map-marker-outline'} color={color} size={SIZES.l} />
+            focused ? 
+              <IconPinFill width={SIZES.l} height={SIZES.l} fill={color} /> 
+              : 
+              <IconPin width={SIZES.l} height={SIZES.l} fill={color} />
           ),
-        }}/>
+        }}
+      />
+      <Tabs.Screen 
+        name="more"
+        options={{ 
+          title: 'More', 
+          tabBarIcon: ({ color, focused }) => (
+            focused ? 
+              <IconPersonFill width={SIZES.l} height={SIZES.l} fill={color} /> 
+              : 
+              <IconPerson width={SIZES.l} height={SIZES.l} fill={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
