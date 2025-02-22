5fbbcc4bcd8183f9759ceff777e00602e4f6aa9a
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-22 18:36:45 +0000

    chore/ imports

--- models/User.ts
+++ models/User.ts
@@ -1,30 +1,30 @@
 import { DocumentData, Timestamp } from "firebase/firestore";
-import { UserProfile } from "../types/UserProfile";
+import { UserProfile } from "@/types/UserProfile";
 
 export interface User {
-    userId: string;
-    userName: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    mobile: string;
-    location: Location;
-    created: Timestamp;
-    profile?: UserProfile;
+  userId: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobile: string;
+  location: Location;
+  created: Timestamp;
+  profile?: UserProfile;
 }
 
 export const mapUserFirestore = (data: DocumentData | undefined): User | null => { 
-    if (!data) return null; 
+  if (!data) return null; 
 
-    return {
-        userId: data.id, 
-        userName: data.userName ?? '',
-        firstName: data.firstName ?? '',
-        lastName: data.lastName ?? '',
-        email: data.email ?? '',
-        mobile: data.mobile ?? '',
-        location: data.location ?? { latitude: 0, longitude: 0 },
-        created: data.created instanceof Timestamp ? data.created : new Timestamp(0, 0),
-        profile: data.profile,
-    };
+  return {
+    userId: data.id, 
+    userName: data.userName ?? '',
+    firstName: data.firstName ?? '',
+    lastName: data.lastName ?? '',
+    email: data.email ?? '',
+    mobile: data.mobile ?? '',
+    location: data.location ?? { latitude: 0, longitude: 0 },
+    created: data.created instanceof Timestamp ? data.created : new Timestamp(0, 0),
+    profile: data.profile,
+  };
 };
