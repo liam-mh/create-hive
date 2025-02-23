578fa90200f61d30b8256c11b12a47df14c9b5f7
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-23 13:15:42 +0000

    feat/ event by type

--- services/eventService.ts
+++ services/eventService.ts
@@ -1,13 +1,26 @@
-import { DocumentData } from "firebase/firestore";
-import { readCollection } from "../hooks/useFirestore";
-import { Event, mapEventFirestore } from "../models/Event";
+import { DocumentData, where } from "firebase/firestore";
+import { queryCollection, readCollection } from "../hooks/useFirestore";
+import { Event, EventType, mapEventFirestore } from "../models/Event";
 
 export async function getEvent(): Promise<Event[]> {
-    try {
-        const eventsData = await readCollection<DocumentData>('event');
-        return eventsData.map(data => mapEventFirestore(data)).filter(event => event !== null) as Event[]; 
-    } catch (error) {
-        console.error("Error getting events:", error);
-        throw error;
-    }
+  try {
+    const eventsData = await readCollection<DocumentData>('event');
+    return eventsData.map(data => mapEventFirestore(data)).filter(event => event !== null) as Event[]; 
+  } catch (error) {
+    console.error("Error getting events:", error);
+    throw error;
+  }
+}
+
+export async function getEventsByType(eventType: EventType): Promise<Event[]> {
+  try {
+    const queryConstraints = [where("eventType", "==", eventType)];
+    const eventsData = await queryCollection<DocumentData>("event", queryConstraints);
+    return eventsData
+      .map((data) => mapEventFirestore(data))
+      .filter((event) => event !== null) as Event[];
+  } catch (error) {
+    console.error(`Error getting events of type ${eventType}:`, error);
+    throw error;
+  }
 }
