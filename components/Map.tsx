7fe8b2682d67c513154a9c5847d9cfecff97862b
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-23 23:19:16 +0000

    feat/ centre selected marker

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,18 +1,17 @@
 import MapView, { Marker, PROVIDER_DEFAULT } from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
+import { useState, useRef } from 'react'; // Import useRef
 import { TEXT, SHADOWS, SIZES } from '@/styles';
-import { Location } from '@/types/Location';
+import { Coordinate } from '@/types/Coordinate';
 import { Region } from 'react-native-maps';
 import { useCityFromCoordinates } from '@/hooks/useCityFromCoordinates';
 import MapFiltersDropdown from './MapFiltersDropdown';
-import CustomMarker from './CustomMarker';
 import MarkerManager from './MarkerManager';
 import useMarkers from '@/hooks/useMarkers';
 
 const Map = () => {
-  const inputLocation: Location = {
+  const inputLocation: Coordinate = {
     latitude: 53.380871,
     longitude: -1.4701,
   };
@@ -26,8 +25,8 @@ const Map = () => {
 
   const [currentRegion, setCurrentRegion] = useState<Region>(initialRegion);
   const city = useCityFromCoordinates(currentRegion.latitude, currentRegion.longitude);
-
   const { markers, loading, error } = useMarkers();
+  const mapRef = useRef<MapView>(null); 
 
   if (loading) {
     return <Text>Loading...</Text>;
@@ -38,15 +37,16 @@ const Map = () => {
   }
 
   return (
-    <View style={{ flex: 1 }}>  
-      <MapView 
-        style={styles.map} 
+    <View style={{ flex: 1 }}>
+      <MapView
+        ref={mapRef} 
+        style={styles.map}
         provider={PROVIDER_DEFAULT}
         initialRegion={initialRegion}
         onRegionChangeComplete={(region) => setCurrentRegion(region)}
         showsPointsOfInterest={false}
-      > 
-        <MarkerManager markers={markers} />
+      >
+        <MarkerManager markers={markers} mapRef={mapRef} /> 
         <SafeAreaView style={[styles.headerContainer, [SHADOWS.containerShadow]]}>
           <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
             <Text style={TEXT.h1}>{city?.toLowerCase()}</Text>
@@ -61,14 +61,14 @@ const Map = () => {
 const styles = StyleSheet.create({
   map: {
     flex: 1,
-    width: '100%', 
+    width: '100%',
     height: '100%',
   },
-  headerContainer: { 
-    flexDirection: "row", 
-    justifyContent: "space-between", 
-    alignItems: "center", 
-    paddingHorizontal: SIZES.m, 
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: SIZES.m,
     paddingVertical: SIZES.s,
   },
 });
--- components/MarkerManager.tsx
+++ components/MarkerManager.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import CustomMarker, { CustomMarkerProps } from '@/components/CustomMarker';
+import MapView, { Region } from 'react-native-maps';
+import { Coordinate } from '@/types/Coordinate';
 
 interface MarkerManagerProps {
   markers: CustomMarkerProps[];
+  mapRef: React.RefObject<MapView>;
 }
 
-const MarkerManager: React.FC<MarkerManagerProps> = ({ markers }) => {
+const MarkerManager: React.FC<MarkerManagerProps> = ({ markers, mapRef }) => {
   const [selectedMarker, setSelectedMarker] = useState<string | null>(null);
-  const handleMarkerPress = (markerKey: string) => {
+
+  const handleMarkerPress = (markerKey: string, coordinate: Coordinate) => { 
     setSelectedMarker(prevSelectedMarker => {
       return prevSelectedMarker === markerKey ? null : markerKey;
     });
+
+    if (mapRef.current) {
+      const region: Region = {
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      };
+      mapRef.current.animateToRegion(region, 350);
+    }
   };
 
   return (
@@ -21,15 +35,15 @@ const MarkerManager: React.FC<MarkerManagerProps> = ({ markers }) => {
         return (
           <CustomMarker
             key={markerKey}
-            coordinate={{ 
-              latitude: marker.coordinate.latitude, 
-              longitude: marker.coordinate.longitude 
+            coordinate={{
+              latitude: marker.coordinate.latitude,
+              longitude: marker.coordinate.longitude,
             }}
             type={marker.type}
             filename={marker.filename}
             text={marker.text}
             isSelected={isSelected}
-            onPress={() => handleMarkerPress(markerKey)}
+            onPress={() => handleMarkerPress(markerKey, marker.coordinate)} 
           />
         );
       })}
