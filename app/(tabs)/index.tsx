4e23537a101e587cccf7696676d0372d57ff982a
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-23 19:02:54 +0000

    chore/ imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,35 @@
 import { StyleSheet, Text, View} from 'react-native';
-import { useRouter, Link } from 'expo-router';
+import { useRouter } from 'expo-router';
 import DisplayAllUsers from '@/components/DisplayAllUsers';
 import DisplayAllEvents from '@/components/DisplayAllEvents';
 import DisplayAllArtwork from '@/components/DisplayAllArtwork';
 
 export default function Index() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Users</Text>
-            <DisplayAllUsers />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Users</Text>
+      <DisplayAllUsers />
 
-            <Text style={styles.text}>Events</Text>
-            <DisplayAllEvents />
+      <Text style={styles.text}>Events</Text>
+      <DisplayAllEvents />
 
-            <Text style={styles.text}>Artwork</Text>
-            <DisplayAllArtwork />
-          
-        </View>
-    );
+      <Text style={styles.text}>Artwork</Text>
+      <DisplayAllArtwork />
+    
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: 'red',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  },
 });
