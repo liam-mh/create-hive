1490243d589075d114073d207f717bf7c30c06b4
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-24 22:26:29 +0000

    feat/ get event by Id

--- services/eventService.ts
+++ services/eventService.ts
@@ -1,6 +1,6 @@
 import { DocumentData, where } from "firebase/firestore";
-import { queryCollection, readCollection } from "../hooks/useFirestore";
-import { Event, EventType, mapEventFirestore } from "../models/Event";
+import { queryCollection, readCollection, readDocument } from "@/hooks/useFirestore";
+import { Event, EventType, mapEventFirestore } from "@/models/Event";
 
 export async function getEvent(): Promise<Event[]> {
   try {
@@ -21,6 +21,17 @@ export async function getEventsByType(eventType: EventType): Promise<Event[]> {
       .filter((event) => event !== null) as Event[];
   } catch (error) {
     console.error(`Error getting events of type ${eventType}:`, error);
+    throw error;
+  }
+}
+
+export async function getEventById(eventId: string): Promise<Event | null> {
+  try {
+    const eventDoc = await readDocument<DocumentData>("event", eventId);
+    const mappedEvent = eventDoc ? mapEventFirestore(eventDoc) : null;
+    return mappedEvent;
+  } catch (error) {
+    console.error(`Error getting artwork for artworkId ${eventId}:`, error);
     throw error;
   }
 }
