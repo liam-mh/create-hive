c08a84c913bbe2a37441993fda76ec0914800261
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-24 21:53:53 +0000

    feat/ refactor map filter

--- components/MapFiltersDropdown.tsx
+++ components/MapFiltersDropdown.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { COLOURS, UNIT } from "@/styles";
+import DetailsPanel from "./DetailsPanel";
 
 import IconVerified from '@/assets/icons/person-check.svg';
 import IconVerifiedFill from '@/assets/icons/person-fill-check.svg';
+import IconArtwork from '@/assets/icons/palette.svg';
+import IconArtworkFill from '@/assets/icons/palette-fill.svg';
 import IconCasual from '@/assets/icons/cup-hot.svg';
 import IconCasualFill from '@/assets/icons/cup-hot-fill.svg';
 import IconWorkshop from '@/assets/icons/brush.svg';
@@ -14,11 +17,51 @@ import IconExhibitionFill from '@/assets/icons/easel2-fill.svg';
 import IconChevronUp from '@/assets/icons/chevron-up.svg';
 import IconChevronDown from '@/assets/icons/chevron-down.svg';
 
+interface FilterItem {
+  filterText: string;
+  icon: React.ComponentType<any>;
+  iconFill: React.ComponentType<any>;
+  filterKey: string;
+}
+
 const MapFiltersDropdown = () => {
   const ICON_SIZE = UNIT * 1.5;
 
   const [expanded, setExpanded] = useState(false);
   const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  const filters: FilterItem[] = [
+    {
+      filterText: "verified only",
+      icon: IconVerified,
+      iconFill: IconVerifiedFill,
+      filterKey: "verified",
+    },
+    {
+      filterText: "hide artwork",
+      icon: IconArtwork,
+      iconFill: IconArtworkFill,
+      filterKey: "artwork",
+    },
+    {
+      filterText: "casual",
+      icon: IconCasual,
+      iconFill: IconCasualFill,
+      filterKey: "casual",
+    },
+    {
+      filterText: "workshop",
+      icon: IconWorkshop,
+      iconFill: IconWorkshopFill,
+      filterKey: "workshop",
+    },
+    {
+      filterText: "exhibition",
+      icon: IconExhibition,
+      iconFill: IconExhibitionFill,
+      filterKey: "exhibition",
+    },
+  ];
 
   const toggleDropdown = () => setExpanded(!expanded);
 
@@ -26,71 +69,100 @@ const MapFiltersDropdown = () => {
     setSelectedFilters((prevSelected) =>
       prevSelected.includes(filter)
         ? prevSelected.filter((item) => item !== filter)
-        : [...prevSelected, filter] 
+        : [...prevSelected, filter]
     );
   };
 
   return (
-    <View style={styles.container}>
-      {expanded ? (
-        <>
-          <TouchableOpacity style={styles.item} onPress={() => toggleFilter("verified")}>
-            {selectedFilters.includes("verified") ? (
-              <IconVerifiedFill width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.primary} />
-            ) : (
-              <IconVerified width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
-            )}
-          </TouchableOpacity>
+    <View style={styles.horizontalContainer}>
+      {/* Filters */}
+      <View style={styles.leftContainer}>
+        {expanded &&
+          filters.map((filter) => (
+            <TouchableOpacity
+              key={filter.filterKey}
+              style={styles.item}
+              onPress={() => toggleFilter(filter.filterKey)}
+            >
+              <View style={styles.filterRow}>
+                  <DetailsPanel text={filter.filterText} isSelected={selectedFilters.includes(filter.filterKey)}/>
+              </View>
+            </TouchableOpacity>
+          ))}
+      </View>
 
-          <TouchableOpacity style={styles.item} onPress={() => toggleFilter("casual")}>
-            {selectedFilters.includes("casual") ? (
-              <IconCasualFill width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.primary} />
-            ) : (
-              <IconCasual width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
-            )}
-          </TouchableOpacity>
+      {/* Icons */}
+      <View style={[styles.container, styles.rightContainer]}>
+        {expanded ? (
+          <>
+            {filters.map((filter) => (
+              <TouchableOpacity
+                key={filter.filterKey}
+                style={styles.item}
+                onPress={() => toggleFilter(filter.filterKey)}
+              >
+                <View style={styles.filterRow}>
+                  {selectedFilters.includes(filter.filterKey) ? (
+                    <filter.iconFill
+                      width={ICON_SIZE}
+                      height={ICON_SIZE}
+                      fill={COLOURS.primary}
+                    />
+                  ) : (
+                    <filter.icon
+                      width={ICON_SIZE}
+                      height={ICON_SIZE}
+                      fill={COLOURS.black}
+                    />
+                  )}
+                </View>
+              </TouchableOpacity>
+            ))}
 
-          <TouchableOpacity style={styles.item} onPress={() => toggleFilter("workshop")}>
-            {selectedFilters.includes("workshop") ? (
-              <IconWorkshopFill width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.primary} />
-            ) : (
-              <IconWorkshop width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
-            )}
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => toggleFilter("exhibition")}>
-            {selectedFilters.includes("exhibition") ? (
-              <IconExhibitionFill width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.primary} />
-            ) : (
-              <IconExhibition width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
-            )}
-          </TouchableOpacity>
-
+            <TouchableOpacity onPress={toggleDropdown}>
+              <IconChevronUp width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
+            </TouchableOpacity>
+          </>
+        ) : (
           <TouchableOpacity onPress={toggleDropdown}>
-            <IconChevronUp width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
+            <IconChevronDown width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
           </TouchableOpacity>
-        </>
-      ) : (
-        <TouchableOpacity onPress={toggleDropdown}>
-          <IconChevronDown width={ICON_SIZE} height={ICON_SIZE} fill={COLOURS.black} />
-        </TouchableOpacity>
-      )}
+        )}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  horizontalContainer: {
+    flexDirection: 'row',
+  },
   container: {
     backgroundColor: COLOURS.white,
     borderRadius: 2,
     alignItems: "center",
     justifyContent: "center",
-    paddingVertical: UNIT,
+    paddingVertical: UNIT / 2,
     paddingHorizontal: UNIT / 2,
     alignSelf: "flex-start",
+    marginRight: UNIT / 2,
+  },
+  rightContainer: {
+    marginRight: 0,
+  },
+  leftContainer: {
+    marginRight: UNIT / 2,
+    alignItems: 'flex-end',
+    paddingVertical: UNIT / 2,
   },
   item: {
     marginBottom: UNIT,
+    height: UNIT * 1.5,
+    justifyContent: 'center',
+  },
+  filterRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
