c77bacdf73ba8e9a079188de404e362b222df397
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-25 13:50:23 +0000

    feat/ buttons and image background

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,8 +1,12 @@
 import { getEventById } from '@/services/eventService';
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { Event } from '@/models/Event';
 import TEXT, { COLOURS, UNIT } from '@/styles';
+import { calculateEventDateTime } from '@/utils/dateTimeUtils';
+import { formatDistrictCity, getAddressFromCoordinates } from '@/utils/locationUtils';
+import { Coordinate } from '@/types/Coordinate';
+import { getImageUrl } from "@/hooks/useFirebaseStorage";
 
 import IconWorkshopFill from '@/assets/icons/brush-fill.svg';
 import IconBookmark from '@/assets/icons/bookmark.svg';
@@ -11,9 +15,8 @@ import IconArtwork from '@/assets/icons/palette.svg';
 import IconCalendar from '@/assets/icons/calendar.svg';
 import IconClock from '@/assets/icons/clock.svg';
 import IconMarker from '@/assets/icons/geo-alt.svg';
-import { calculateEventDateTime } from '@/utils/dateTimeUtils';
-import { formatDistrictCity, getAddressFromCoordinates } from '@/utils/locationUtils';
-import { Coordinate } from '@/types/Coordinate';
+import InformationButton from './buttons/InformationButton';
+import RegisterButton from './buttons/RegisterButton';
 
 interface EventCardProps {
   eventId: string;
@@ -23,7 +26,8 @@ const EventCard: React.FC<EventCardProps> = ({ eventId }) => {
   const [event, setEvent] = useState<Event | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [eventLocation, setEventLocation] = useState<string | null>(null); 
+  const [eventLocation, setEventLocation] = useState<string | null>(null);
+  const [imageUri, setImageUri] = useState<string | null>(null);
 
   const eventTypeIconSize = UNIT * 1.5;
   const iconSize = UNIT;
@@ -42,8 +46,10 @@ const EventCard: React.FC<EventCardProps> = ({ eventId }) => {
           const address = await getAddressFromCoordinates(coordinate);
           const formattedAddress = formatDistrictCity(address);
           setEventLocation(formattedAddress);
-        }
 
+          const url = await getImageUrl('event', eventId);
+          setImageUri(url);
+        }
       } catch (err) {
         setError('Failed to load event.');
         console.error(err);
@@ -82,44 +88,60 @@ const EventCard: React.FC<EventCardProps> = ({ eventId }) => {
   const eventDateTime = calculateEventDateTime(event.start, event.end);
 
   return (
-    <View style={styles.contentContainer}>
-      <View style={styles.titleContainer}>
-        <View style={styles.innerRow}>
-          <Text style={TEXT.h1}>{event.eventType}</Text>
-          <IconWorkshopFill width={eventTypeIconSize} height={eventTypeIconSize} fill={COLOURS.secondary} />
+    <ImageBackground
+      source={{ uri: imageUri || undefined }} 
+      style={styles.backgroundImage}
+    >
+      <View style={styles.overlay} />
+      <View style={styles.contentContainer}>
+        <View style={styles.titleContainer}>
+          <View style={styles.innerRow}>
+            <Text style={TEXT.h1}>{event.eventType}</Text>
+            <IconWorkshopFill width={eventTypeIconSize} height={eventTypeIconSize} fill={COLOURS.secondary} />
+          </View>
+          <View style={styles.innerRow}>
+            {!event.private && <IconLock width={iconSize} height={iconSize} fill={COLOURS.black} />}
+            <IconBookmark width={iconSize} height={iconSize} fill={COLOURS.black} />
+          </View>
         </View>
-        <View style={styles.innerRow}>
-          {!event.private && (<IconLock width={iconSize} height={iconSize} fill={COLOURS.black} />)}
-          <IconBookmark width={iconSize} height={iconSize} fill={COLOURS.black} />
-        </View>
-      </View>
 
-      <View style={styles.detailsContainer}>
-        <View style={styles.innerRow}>
-          <IconArtwork width={iconSize} height={iconSize} fill={COLOURS.black} />
-          <Text style={TEXT.regular}>{event.medium.primary} - {event.medium.secondary}</Text>
+        <View style={styles.detailsContainer}>
+          <View style={styles.innerRow}>
+            <IconArtwork width={iconSize} height={iconSize} fill={COLOURS.black} />
+            <Text style={TEXT.regular}>{event.medium.primary} - {event.medium.secondary}</Text>
+          </View>
+          <View style={styles.innerRow}>
+            <IconCalendar width={iconSize} height={iconSize} fill={COLOURS.black} />
+            <Text style={TEXT.regular}>{eventDateTime.date}</Text>
+          </View>
+          <View style={styles.innerRow}>
+            <IconClock width={iconSize} height={iconSize} fill={COLOURS.black} />
+            <Text style={TEXT.regular}>{eventDateTime.time}</Text>
+          </View>
+          <View style={styles.innerRow}>
+            <IconMarker width={iconSize} height={iconSize} fill={COLOURS.black} />
+            <Text style={TEXT.regular}>{eventLocation?.toLocaleLowerCase()}</Text>
+          </View>
         </View>
-        <View style={styles.innerRow}>
-          <IconCalendar width={iconSize} height={iconSize} fill={COLOURS.black} />
-          <Text style={TEXT.regular}>{eventDateTime.date}</Text>
-        </View>
-        <View style={styles.innerRow}>
-          <IconClock width={iconSize} height={iconSize} fill={COLOURS.black} />
-          <Text style={TEXT.regular}>{eventDateTime.time}</Text>
-        </View>
-        <View style={styles.innerRow}>
-          <IconMarker width={iconSize} height={iconSize} fill={COLOURS.black} />
-          <Text style={TEXT.regular}>{eventLocation?.toLocaleLowerCase()}</Text>
+
+        <View style={styles.buttonsContainer}>
+          <InformationButton type={'event'} id={event.eventId} />
+          <RegisterButton id={event.eventId} />
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', 
+  },
   contentContainer: {
-    flex: 1,
-    backgroundColor: COLOURS.offwhite,
     padding: UNIT,
     gap: UNIT,
   },
@@ -134,6 +156,10 @@ const styles = StyleSheet.create({
   },
   detailsContainer: {
     gap: UNIT / 2,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    gap: UNIT,
   },
 });
 
