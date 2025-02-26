2b98e5bb7a1cd049e969a6f4db3479646446f774
Liam <33222279+liam-mh@users.noreply.github.com>
2025-02-26 17:28:49 +0000

    feat/ use base Service sub func

--- services/attendeeService.ts
+++ services/attendeeService.ts
@@ -1,44 +1,17 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { Attendee, mapAttendeeFirestore } from "@/models/Attendee";
-import { db } from "@/config/firebase";
+import { BaseService } from './baseService';
+import { Attendee, mapAttendeeFirestore } from '@/models/Attendee';
+import { where } from 'firebase/firestore';
 
-const collectionName = 'attendee';
+export function createAttendeeService(eventId: string) {
+  return new BaseService<Attendee>('attendee', mapAttendeeFirestore, eventId);
+}
 
 export async function getAttendeesByEventId(eventId: string): Promise<Attendee[]> {
-  try {
-    const attendeesCollectionRef = collection(db, 'attendee', eventId, 'attendee');
-    const q = query(attendeesCollectionRef);
-    const querySnapshot = await getDocs(q);
-    const attendees: Attendee[] = [];
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      const mappedAttendee = mapAttendeeFirestore(doc.data());
-      if(mappedAttendee){
-        attendees.push(mappedAttendee);
-      }
-    });
-    return attendees;
-  } catch (error) {
-    console.error(`Error getting attendees for eventId ${eventId}:`, error);
-    throw error;
-  }
+  const attendeeService = createAttendeeService(eventId);
+  return attendeeService.get();
 }
 
 export async function getAttendeeByUserIdAndEventId(eventId: string, userId: string): Promise<Attendee | null> {
-  try {
-    const attendeesCollectionRef = collection(db, 'attendee', eventId, 'attendee');
-    const q = query(attendeesCollectionRef, where('attendeeId', '==', userId)); 
-    const querySnapshot = await getDocs(q);
-
-    if (querySnapshot.size > 0) {
-      const doc = querySnapshot.docs[0];
-      const mappedAttendee = mapAttendeeFirestore(doc.data());
-      return mappedAttendee || null;
-    } else {
-      return null; 
-    }
-  } catch (error) {
-    console.error(`Error getting attendee for userId ${userId} and eventId ${eventId}:`, error);
-    return null;
-  }
+  const attendeeService = createAttendeeService(eventId);
+  return attendeeService.get([where('attendeeId', '==', userId)]).then(attendees => attendees[0] || null);
 }
