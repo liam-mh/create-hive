d05df5aaec039a9489c199ecce4fd57f1006b59c
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-05 13:51:13 +0000

    feat/ privacy icon

---
+++ components/buttons/EventPrivacyIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import BaseButton from '@/components/buttons/BaseButton';
+import IconLock from '@/assets/icons/lock.svg';
+
+interface EventPrivacyIconProps {
+  isPrivate: boolean;
+}
+
+const InformationButton: React.FC<EventPrivacyIconProps> = ({ isPrivate = false }) => {
+  const handlePress = () => {};
+
+  if (!isPrivate) {
+    return (
+      <BaseButton
+        text='private'
+        icon={IconLock}
+        iconFill={IconLock}
+        pending={false}
+        onPress={handlePress}
+        isSelected={false}
+        isIconButton={true}
+      />
+    );
+  } else {
+    null;
+  }
+};
+
+export default InformationButton;
