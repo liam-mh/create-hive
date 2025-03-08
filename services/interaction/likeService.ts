ee647ae81f7dd34c7367bfe821189c3c15a958bb
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-08 16:55:56 +0000

    feat/ like service

--- services/interaction/likeService.ts
+++ services/interaction/likeService.ts
@@ -1,8 +1,20 @@
+import { Timestamp } from 'firebase/firestore';
 import { Interaction, ItemType, InteractionType } from '@/models/Interaction';
-import { addInteraction } from './interactionService';
+import { InteractionServicePost, addInteraction, getInteraction } from '@/services/interaction/interactionService';
 
 const type: InteractionType = 'like';
 
 export async function likeItem(userId: string, itemId: string, itemType: ItemType): Promise<Interaction | null> {
-  return addInteraction(userId, itemId, itemType, type);
+  const like: InteractionServicePost = {
+    userId: userId,
+    itemId: itemId,
+    itemType: itemType,
+    actionType: type,
+    timestamp: Timestamp.now(),
+  }
+  return addInteraction(like);
+}
+
+export async function getLike(userId: string, itemId: string, itemType: ItemType) {
+  return getInteraction(userId, itemType, itemId, type);
 }
