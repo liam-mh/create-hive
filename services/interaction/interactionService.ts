be5eba8003913654446bd7f85c26cda645868fac
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-08 16:55:35 +0000

    fix/ working and improved add + get

--- services/interaction/interactionService.ts
+++ services/interaction/interactionService.ts
@@ -1,75 +1,22 @@
-import { Timestamp, where } from 'firebase/firestore';
-import { InteractionType, Interaction, ItemType, mapInteractionFirestore } from '@/models/Interaction';
-import { BaseService } from '../baseService';
+import { Interaction, InteractionType, ItemType, mapInteractionFirestore } from '@/models/Interaction';
+import { BaseService } from '@/services/baseService';
+import { where } from 'firebase/firestore';
 
 const collectionName = 'interaction';
+export type InteractionServicePost = Omit<Interaction, 'id'>; 
 
-export async function addInteraction(
-  userId: string,
-  itemId: string,
-  itemType: ItemType,
-  actionType: InteractionType,
-  content: string | null = null
-): Promise<Interaction | null> {
-  try {
-    const interaction: Omit<Interaction, 'id'> = {
-      userId,
-      itemId,
-      itemType,
-      actionType,
-      content,
-      timestamp: Timestamp.now(),
-    };
-
-    const userService = new BaseService<Interaction>(collectionName, mapInteractionFirestore, userId);
-    const itemTypeService = userService.getSubcollectionService<Interaction>(itemType, mapInteractionFirestore);
-
-    return itemTypeService.create(interaction);
-  } catch (error) {
-    console.error(
-      `Error adding interaction for userId ${userId}, itemId ${itemId}, itemType ${itemType}, actionType ${actionType}:`,
-      error
-    );
-    return null;
-  }
+export function createService(userId: string, itemType: ItemType)  {
+  return new BaseService<Interaction>(collectionName, mapInteractionFirestore, userId, itemType);
 }
 
-export async function getInteractionsByUserAndType(userId: string, itemType: ItemType): Promise<Interaction[]> {
-  const userService = new BaseService<Interaction>(collectionName, mapInteractionFirestore, userId);
-  const itemTypeService = userService.getSubcollectionService<Interaction>(itemType, mapInteractionFirestore);
-  return itemTypeService.get();
+export async function addInteraction(interaction: InteractionServicePost): Promise<Interaction | null> {
+  const service = createService(interaction.userId, interaction.itemType);
+  return service.create(interaction);
 }
 
-export async function getInteractionsByUserAndItem(
-  userId: string,
-  itemType: ItemType,
-  itemId: string
+export async function getInteraction(
+  userId: string, itemType: ItemType, itemId: string, interactionType: InteractionType 
 ): Promise<Interaction[]> {
-  const userService = new BaseService<Interaction>(collectionName, mapInteractionFirestore, userId);
-  const itemTypeService = userService.getSubcollectionService<Interaction>(itemType, mapInteractionFirestore);
-  return itemTypeService.get([where('itemId', '==', itemId)]);
-}
-
-export async function getAllInteractionsByUser(userId: string): Promise<Interaction[]> {
-  const interactionTypes: ItemType[] = ['event', 'artwork', 'comment', 'user'];
-  let allInteractions: Interaction[] = [];
-
-  for (const itemType of interactionTypes) {
-    const interactions = await getInteractionsByUserAndType(userId, itemType);
-    allInteractions = allInteractions.concat(interactions);
-  }
-
-  return allInteractions;
-}
-
-export async function getAllInteractionsByUserAndItemId(userId: string, itemId: string): Promise<Interaction[]> {
-  const interactionTypes: ItemType[] = ['event', 'artwork', 'comment', 'user'];
-  let allInteractions: Interaction[] = [];
-
-  for (const itemType of interactionTypes) {
-    const interactions = await getInteractionsByUserAndItem(userId, itemType, itemId);
-    allInteractions = allInteractions.concat(interactions);
-  }
-
-  return allInteractions;
-}
+  const service = createService(userId, itemType);
+  return service.get([where('itemId', '==', itemId), where('actionType', '==', interactionType)]);
+} 
