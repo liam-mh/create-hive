ed2170d8bd36bdae49a02525d3ee19ca82d296d6
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-08 17:56:33 +0000

    feat/ save service

--- services/interaction/saveService.ts
+++ services/interaction/saveService.ts
@@ -1,8 +1,24 @@
+import { Timestamp } from 'firebase/firestore';
 import { Interaction, ItemType, InteractionType } from '@/models/Interaction';
-import { addInteraction } from '@/services/interaction/interactionService';
+import { InteractionServicePost, addInteraction, getInteraction } from '@/services/interaction/interactionService';
 
 const type: InteractionType = 'save';
+export interface SaveServiceProps {
+  userId: string;
+  itemId: string;
+  itemType: ItemType;
+}
 
-export async function saveItem(userId: string, itemId: string, itemType: ItemType): Promise<Interaction | null> {
-  return addInteraction(userId, itemId, itemType, type);
+export async function saveItem(saveServiceProps: SaveServiceProps): Promise<Interaction | null> {
+  const save: InteractionServicePost = {
+    ...saveServiceProps,
+    actionType: type,
+    timestamp: Timestamp.now(),
+  }
+  return addInteraction(save);
+}
+
+export async function getSave(saveServiceProps: SaveServiceProps): Promise<Interaction | null> {
+  const { userId, itemType, itemId } = saveServiceProps;
+  return getInteraction(userId, itemType, itemId, type);
 }
