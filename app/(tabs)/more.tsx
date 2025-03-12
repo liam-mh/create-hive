8f35bd3a64b383f882924087f15c76e49b9b0660
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-12 16:57:21 +0000

    feat/ Profile card and view model

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,10 +1,14 @@
+import ProfileCard from '@/components/ProfileCard';
+import { UNIT } from '@/styles';
 import { StyleSheet, View, SafeAreaView, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function More() {
   return (
     <View style={styles.container}>
-      <Text>More</Text>
+      <View style={styles.profile}>
+        <ProfileCard userId='FghLfeUlFYO0RMZYjzI3' />
+      </View>
     </View>
   );
 }
@@ -12,5 +16,9 @@ export default function More() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
+    padding: UNIT
   },
+  profile: {
+    marginBottom: UNIT
+  }
 });
