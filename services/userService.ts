df2ecf7dc1ece059b6d6555dda85eefbfb118dfe
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-12 16:56:48 +0000

    feat/ user profile service

--- services/userService.ts
+++ services/userService.ts
@@ -1,6 +1,8 @@
+import { mapUserProfileFirestore, UserProfile } from '@/models/UserProfile';
 import { BaseService } from './baseService'; 
 import { User, mapUserFirestore } from '@/models/User';
 
+// User Service
 export const userService = new BaseService<User>('user', mapUserFirestore);
 
 export async function getUser(): Promise<User[]> {
@@ -9,4 +11,15 @@ export async function getUser(): Promise<User[]> {
 
 export async function getUserById(id: string): Promise<User | null> {
   return userService.getById(id);
+}
+
+// UserProfile Service
+export const userProfileService = new BaseService<UserProfile>('userProfile', mapUserProfileFirestore);
+
+export async function getUserProfileById(id: string): Promise<UserProfile | null> {
+  return userProfileService.getById(id);
+}
+
+export async function createUserProfile(id: string, data: UserProfile): Promise<UserProfile | null> {
+  return userProfileService.createById(id, data);
 }
