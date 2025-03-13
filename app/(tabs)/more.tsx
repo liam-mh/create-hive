10d9963533398b87569fa39c0b35bd9ac1f3bdc2
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-13 17:16:18 +0000

    feat/ scroll view on more page

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,31 +1,37 @@
 import ProfileCard from '@/components/ProfileCard';
 import ProfileTabSelector from '@/components/ProfileTabSelector';
 import { COLOURS, UNIT } from '@/styles';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 export default function More() {
   return (
-    <View style={styles.container}>
-      <View style={styles.sectionContainer}>
-        <ProfileCard userId='FghLfeUlFYO0RMZYjzI3' />
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.sectionContainer}>
+          <ProfileCard userId='FghLfeUlFYO0RMZYjzI3' />
+        </View>
+        <View style={styles.div} />
+        <View style={styles.sectionContainer}>
+          <ProfileTabSelector />
+        </View>
       </View>
-      <View style={styles.div}/>
-      <ProfileTabSelector />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: COLOURS.white,
-    gap: UNIT
+  },
+  content: {
+    gap: UNIT,
+    flexGrow: 1,
   },
   sectionContainer: {
-    paddingInline: UNIT
+    paddingInline: UNIT,
   },
   div: {
     height: 1,
-    backgroundColor: COLOURS.offwhite
+    backgroundColor: COLOURS.offwhite,
   },
 });
