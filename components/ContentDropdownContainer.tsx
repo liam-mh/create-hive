96795185412a594306037c22d67139d6114438e8
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-13 17:41:28 +0000

    feat/ toggle container

---
+++ components/ContentDropdownContainer.tsx
@@ -0,0 +1,54 @@
+import TEXT, { COLOURS, UNIT } from '@/styles';
+import { getIcon } from '@/utils/iconUtils';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+
+interface ContentDropdownContainerProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const ContentDropdownContainer: React.FC<ContentDropdownContainerProps> = ({ title, children }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const iconName = isExpanded ? 'chevronUp' : 'chevronDown';
+  const icon = getIcon(iconName, undefined, COLOURS.primary);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.header} onPress={toggleDropdown}>
+        <Text style={TEXT.boldPrimary}>{title}</Text>
+        {icon}
+      </TouchableOpacity>
+      {isExpanded && (
+        <ScrollView style={styles.contentContainer}>
+          <View>
+            {children}
+          </View>
+        </ScrollView>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    overflow: 'hidden',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: UNIT,
+  },
+  contentContainer: {
+    overflow: 'hidden',
+  },
+});
+
+export default ContentDropdownContainer;
