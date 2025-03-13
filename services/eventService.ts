7cd2c13028df560e64bb0adc7f8d820f189b5ca2
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-13 17:13:04 +0000

    feat/ get by pagination and user id

--- services/eventService.ts
+++ services/eventService.ts
@@ -1,6 +1,6 @@
 import { BaseService } from './baseService';
 import { Event, mapEventFirestore, EventType } from '@/models/Event';
-import { where } from 'firebase/firestore';
+import { DocumentData, QueryDocumentSnapshot, where } from 'firebase/firestore';
 
 export type EventServicePost = Omit<Event, 'eventId'>; 
 
@@ -16,6 +16,19 @@ export async function getEvent(): Promise<Event[]> {
 
 export async function getEventById(id: string): Promise<Event | null> {
   return eventService.getById(id);
+}
+
+export async function getEventsByUserId(userId: string): Promise<Event[]> {
+  return eventService.get([where('userId', '==', userId)]);
+}
+
+export async function getEventsByUserIdPaginated(
+  userId: string,
+  pageSize: number,
+  lastDocument?: QueryDocumentSnapshot<DocumentData>
+): Promise<{ events: Event[]; lastDocument: QueryDocumentSnapshot<DocumentData> | null }> {
+  const result = await eventService.getPaginated([where('userId', '==', userId)], pageSize, lastDocument);
+  return { events: result.data, lastDocument: result.lastDocument };
 }
 
 export async function getEventsByType(eventType: EventType): Promise<Event[]> {
