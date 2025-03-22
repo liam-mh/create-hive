cb99e6672bafcd9562ad60a832c8ae0a28f4f0aa
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-22 14:51:36 +0000

    feat/ visit profile button

---
+++ components/buttons/VisitProfileButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import BaseButton from '@/components/buttons/BaseButton';
+import { useRouter } from 'expo-router';
+
+interface VisitProfileButtonProps {
+  id: string;
+}
+
+const VisitProfileButton: React.FC<VisitProfileButtonProps> = ( props ) => {
+  const router = useRouter();
+
+  const handlePress = () => {
+    router.push({
+      pathname: '/(tabs)/more',
+      params: {
+        userId: props.id
+      }
+    });
+  };
+
+  return (
+    <BaseButton
+      text='visit artist profile'
+      icon='person'
+      iconFill='personFill'
+      pending={false}
+      onPress={handlePress}
+      isSelected={false}
+      isIconButton={false}
+    />
+  );
+};
+
+export default VisitProfileButton;
