1b4e6a61790306787e12921af705da579e2a47b0
Liam <33222279+liam-mh@users.noreply.github.com>
2025-03-28 17:20:58 +0000

    feat/ editable state and medium tags

--- components/buttons/TagButton.tsx
+++ components/buttons/TagButton.tsx
@@ -1,34 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { TextInput, View, StyleSheet, Text } from 'react-native';
 import BaseButton from '@/components/buttons/BaseButton';
 import { useRouter } from 'expo-router';
+import { COLOURS, CORNERS, UNIT } from '@/styles';
 
 interface TagButtonProps {
   tag: string;
+  edit?: boolean;
+  onEdit?: (text: string) => void;
+  isMedium?: boolean;
 }
 
-const TagButton: React.FC<TagButtonProps> = ( props ) => {
+const TagButton: React.FC<TagButtonProps> = (props) => {
   const router = useRouter();
+  const [tagText, setTagText] = useState<string>(props.tag);
+  const inputRef = useRef<TextInput>(null);
+
+  useEffect(() => {
+    if (props.edit && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [props.edit]);
 
   const handlePress = () => {
     router.push({
       pathname: '/search',
       params: {
-        tag: props.tag,
-      }
+        tag: tagText,
+      },
     });
   };
 
-  return (
-    <BaseButton
-      text={props.tag}
-      icon='tag'
-      iconFill='tagFill'
-      pending={false}
-      onPress={handlePress}
-      isSelected={false}
-      isIconButton={false}
-    />
-  );
+  const handleTextChange = (text: string) => {
+    setTagText(text);
+    if (props.onEdit) {
+      props.onEdit(text);
+    }
+  };
+
+  if (!props.edit) {
+    return (
+      <BaseButton
+        text={tagText}
+        icon="tag"
+        iconFill="tagFill"
+        pending={false}
+        onPress={handlePress}
+        isSelected={false}
+        isIconButton={false}
+      />
+    );
+  } else {
+    if (!props.isMedium) {
+      return (
+        <View style={styles.textInput}>
+          <TextInput
+            ref={inputRef}
+            value={tagText}
+            onChangeText={handleTextChange}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={[styles.textInput, {borderColor: COLOURS.primary}]}>
+          <Text >{tagText}</Text>
+        </View>
+      )
+    }
+  }
 };
+
+const styles = StyleSheet.create({
+  textInput: {
+    flexDirection: 'row',
+    gap: UNIT,
+    borderWidth: 2,
+    borderColor: COLOURS.offwhite,
+    borderRadius: CORNERS.default,
+    padding: UNIT,
+  },
+});
 
 export default TagButton;
