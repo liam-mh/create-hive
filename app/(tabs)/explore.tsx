6b17dd2dac783cd3c1b463881ada5c844b9bbd74
Liam <33222279+liam-mh@users.noreply.github.com>
2025-04-11 01:07:18 +0100

    feat/ user location

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet, View, SafeAreaView, Text } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StyleSheet, View } from 'react-native';
 import Map from '@/components/Map';
+import { useAuth } from '@/context/authContext';
 
 export default function Explore() {
+  const user = useAuth().user;
+  if (!user) return null;
+  
   return (
     <View style={styles.container}>
-      <Map />
+      <Map inputLocation={user.location} />
     </View>
   );
 }
