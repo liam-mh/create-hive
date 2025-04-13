78d9756cd27c1625281a2e8d55ed238ccb56e805
Liam <33222279+liam-mh@users.noreply.github.com>
2025-04-13 18:30:25 +0100

    feat/ white fonts

--- styles.js
+++ styles.js
@@ -106,6 +106,11 @@ export const TEXT = StyleSheet.create({
     fontSize: SIZES.m,
     color: COLOURS.black,
   },
+  boldWhite: {
+    fontFamily: FONTS.bold,
+    fontSize: SIZES.m,
+    color: COLOURS.white,
+  },
   boldGrey: {
     fontFamily: FONTS.bold,
     fontSize: SIZES.m,
@@ -121,6 +126,16 @@ export const TEXT = StyleSheet.create({
     fontSize: SIZES.xl,
     color: COLOURS.primary,
     letterSpacing: SPACING.reduced,
+  },
+  h1White: {
+    fontFamily: FONTS.bold,
+    fontSize: SIZES.xl,
+    color: COLOURS.white,
+  },
+  h1Black: {
+    fontFamily: FONTS.bold,
+    fontSize: SIZES.xl,
+    color: COLOURS.black,
   },
   h2: {
     fontFamily: FONTS.bold, 
